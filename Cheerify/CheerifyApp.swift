4e5dc62364fa7b48b5e8ccf464ae1c88621f68c3
Euie Son <sonchristina@utexas.edu>
2024-01-05 14:16:30 -0600

    Initial Commit

---
+++ Cheerify/CheerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerifyApp.swift
+//  Cheerify
+//
+//  Created by Yerim Son on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CheerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
